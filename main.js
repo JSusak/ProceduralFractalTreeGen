4b2997dd82b7361f47b86401d5450de7ce63a84f
Josh <joshuasusak@gmail.com>
2022-07-17 13:10:40 +0100

    Added further logic to the JS script and main HTML.

--- main.js
+++ main.js
@@ -4,6 +4,17 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
+/**
+ * Draw a procedurally generated tree, based on given parameters.
+ * @param {*} x The starting x position of the tree.
+ * @param {*} y
+ * @param {*} length
+ * @param {*} angle
+ * @param {*} width
+ * @param {*} col1
+ * @param {*} col2
+ * @returns
+ */
 function draw(x, y, length, angle, width, col1, col2) {
   ctx.beginPath();
   ctx.save();
@@ -13,17 +24,29 @@ function draw(x, y, length, angle, width, col1, col2) {
   ctx.translate(x, y);
   ctx.rotate((angle * Math.PI) / 180);
   ctx.moveTo(0, 0);
-  ctx.lineTo(0, -length);
+  //ctx.lineTo(0, -length);
+  if (angle > 0) {
+    ctx.quadraticCurveTo(10, -length / 2, 0, -length);
+  } else {
+    ctx.quadraticCurveTo(-10, -length / 2, 0, -length);
+  }
   ctx.stroke();
 
   if (length < 10) {
+    ctx.beginPath();
+    //Third parameter is leaf size.
+    ctx.arc(0, -length, 7, 50, Math.PI / 2);
+    ctx.fill();
     ctx.restore();
+
     return;
   }
 
   //Changing x in the angle changes the overall layout of the tree.
-  draw(0, -length, length * 0.75, angle + 150, width, col1, col2);
-  draw(0, -length, length * 0.75, angle - 5, width, col1, col2);
+  //For each recursive call, the branch width will go thinner.
+  draw(0, -length, length * 0.75, angle + 30, width * 0.7, col1, col2);
+  draw(0, -length, length * 0.75, angle - 30, width * 0.6, col1, col2);
   ctx.restore();
 }
-draw(canvas.width / 2, canvas.height - 80, 220, 0, 1, "white", "red");
+//Col 1 is branch colour, col 2 is leaf colour.
+draw(canvas.width / 2, canvas.height - 80, 220, 0, 10, "grey", "pink");
