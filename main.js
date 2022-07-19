716d5d2ee3eac4d0afc8b377d0f3661136fd2374
Josh <joshuasusak@gmail.com>
2022-07-19 09:47:09 +0100

    Added button event listener.

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const canvas = document.querySelector("canvas");
-const button = document.querySelector(".generate");
+const button = document.querySelector(".generateNormal");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
@@ -19,6 +19,8 @@ function draw(x, y, length, angle, width, col1, col2) {
   ctx.beginPath();
   ctx.save();
   ctx.strokeStyle = col1;
+  ctx.shadowBlur = 5;
+  ctx.shadowColor = col1;
   ctx.fillStyle = col2;
   ctx.lineWidth = width;
   ctx.translate(x, y);
@@ -49,4 +51,21 @@ function draw(x, y, length, angle, width, col1, col2) {
   ctx.restore();
 }
 //Col 1 is branch colour, col 2 is leaf colour.
+button.addEventListener("click", generateNewTree);
 draw(canvas.width / 2, canvas.height - 80, 220, 0, 10, "grey", "pink");
+
+function generateNewTree() {
+  //Clears current tree from view.
+  clearCanvas();
+  draw(canvas.width / 2, canvas.height - 80, 20, 0, 10, "white", "pink");
+}
+
+/**
+ * Removes all visible content from the canvas, sets up initialisation of new tree.
+ */
+function clearCanvas() {
+  ctx.beginPath();
+  ctx.fillStyle = "rgba(0, 0, 0, 255)";
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
+  ctx.stroke();
+}
