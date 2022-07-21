a825b1fc5e2c48f6e0efa78f320f6cd6857de77c
Josh <joshuasusak@gmail.com>
2022-07-21 08:42:30 +0100

    Updated main logic (removed redundant code).

--- main.js
+++ main.js
@@ -4,26 +4,15 @@ canvas.height = window.innerHeight;
 const ctx = canvas.getContext("2d");
 
 //
-let opacityValue = document.getElementById("opacity").value;
-let treeWidth = document.getElementById("tWidth").value;
-let treeLength = document.getElementById("tLength").value;
-let treeAngle = document.getElementById("tAngle").value;
-let branchAngle = document.getElementById("bAngle").value;
-let leafSize = document.getElementById("lSize").value;
-let branchColour = document.getElementById("bCol").value;
-let leafColour = document.getElementById("lCol").value;
+let opacityValue = document.getElementById("opacity");
+let treeWidth = document.getElementById("tWidth");
+let treeLength = document.getElementById("tLength");
+let treeAngle = document.getElementById("tAngle");
+let branchAngle = document.getElementById("bAngle");
+let leafSize = document.getElementById("lSize");
+let branchColour = document.getElementById("bCol");
+let leafColour = document.getElementById("lCol");
 let bezier = document.getElementById("checkbezier");
-
-function updateSliderValues() {
-  opacityValue = document.getElementById("opacity").value;
-  treeWidth = document.getElementById("tWidth").value;
-  treeLength = document.getElementById("tLength").value;
-  treeAngle = document.getElementById("tAngle").value;
-  branchAngle = document.getElementById("bAngle").value;
-  leafSize = document.getElementById("lSize").value;
-  branchColour = document.getElementById("bCol").value;
-  leafColour = document.getElementById("lCol").value;
-}
 /**
  * Draw a procedurally generated tree, based on given parameters.
  * @param {*} x The starting x position of the tree.
@@ -39,11 +28,11 @@ function draw(x, y, length, angle, width) {
   //Retrieve slider values (which may have changed from last drawing)
   ctx.beginPath();
   ctx.save();
-  ctx.globalAlpha = opacityValue;
-  ctx.strokeStyle = branchColour;
+  ctx.globalAlpha = opacityValue.value;
+  ctx.strokeStyle = branchColour.value;
   ctx.shadowBlur = 15;
-  ctx.shadowColor = leafColour;
-  ctx.fillStyle = leafColour;
+  ctx.shadowColor = leafColour.value;
+  ctx.fillStyle = leafColour.value;
   ctx.lineWidth = width;
   ctx.translate(x, y);
   ctx.rotate((angle * Math.PI) / 180);
@@ -61,38 +50,36 @@ function draw(x, y, length, angle, width) {
   if (length < 10) {
     ctx.beginPath();
     //Third parameter is leaf size.
-    ctx.arc(0, -length, leafSize, 50, Math.PI / 2);
+    ctx.arc(0, -length, leafSize.value, 50, Math.PI / 2);
     ctx.fill();
     ctx.restore();
 
     return;
   }
-  curve = branchAngle * 10 + 10;
+  curve = branchAngle.value * 10 + 10;
 
   //Changing x in the angle changes the overall layout of the tree.
   //For each recursive call, the branch width will go thinner.
-  draw(0, -length, length * 0.75, angle + branchAngle, width * 0.7);
-  draw(0, -length, length * 0.75, angle - branchAngle, width * 0.6);
+  draw(0, -length, length * 0.75, angle + branchAngle.value, width * 0.7);
+  draw(0, -length, length * 0.75, angle - branchAngle.value, width * 0.6);
   ctx.restore();
 }
 draw(canvas.width / 2, canvas.height - 80, 300, 10, 10);
 
 function generateNewTree() {
   //Clears current tree from view.
-  updateSliderValues();
   clearCanvas();
   draw(
     canvas.width / 2,
     canvas.height - 80,
-    treeLength,
-    treeAngle,
-    treeWidth,
-    branchColour,
-    leafColour,
-    leafSize
+    treeLength.value,
+    treeAngle.value,
+    treeWidth.value,
+    branchColour.value,
+    leafColour.value,
+    leafSize.value
   );
 }
-
 /**
  * Removes all visible content from the canvas, sets up initialisation of new tree.
  */
